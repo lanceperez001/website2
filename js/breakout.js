fc73272ee6cad71908bfa36789e99c975773fe05
Lance Perez <142560197+lanceperez001@users.noreply.github.com>
2024-04-02 16:51:26 +0000

    Tue, Apr 2, 2024, 9:51 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -132,6 +132,28 @@ function keyUp(e) {
 document.addEventListener('keydown', keyDown)
 document.addEventListener('keyup', keyUp)
 
+function moveBall() {
+    ball.x = ball.x + ball.dx
+    ball.y = ball.y + ball.dy
+
+    if (ball.y + ball.size < 0) {
+        ball.dy = -1 * ball.dy
+    }
+
+    if (ball.x + ball.size > canvas.width) {
+        ball.dx = -1 * ball.dx
+    }
+
+    if (ball.y + ball.size > canvas.height) {
+        ball.dy = -1 * ball.dy
+    }
+
+    if (ball.x + ball.size < 0) {
+        ball.dx = -1 * ball.dx
+    }
+
+}
+
 // Update the canvas drawing and animation
 function update() {
     movePaddle()
