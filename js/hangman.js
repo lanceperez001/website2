9df5bdf26f1fb8124134909537466fb07ffa602f
Lance Perez <142560197+lanceperez001@users.noreply.github.com>
2024-05-21 19:49:37 -0700

    Update hangman.js

--- js/hangman.js
+++ js/hangman.js
@@ -1,77 +1,116 @@
 const wordEl = document.getElementById('word')
-wrongLettersEl = document.getElementById('wrong-letters')
+const wrongLettersEl = document.getElementById('wrong-letters')
 const playAgainBtn = document.getElementById('play-button')
 const popup = document.getElementById('popup-container')
 const notification = document.getElementById('notification-container')
 const finalMessage = document.getElementById('final-message')
 const figureParts = document.querySelectorAll('.figure-part')
 
+
 const word = ['application', 'programming', 'interface', 'wizard']
 
+let gameEnding = false;
 let selectedIndex = Math.floor(word.length * Math.random())
 let selectedWord = word[selectedIndex]
 
 const correctLetters = []
 const wrongLetters = []
 
-//show hidden words
-function displayWord() {
+//show hidden word
+function displayWord(){
     wordEl.innerHTML = `
     ${selectedWord
         .split('')
         .map(letter => `
-         <span class="letter">
-         ${correctLetters.includes(letter) ? letter : ''}
-         </span>
-        `).join('')
-    }
-    `
-    const innerWord = wordEl.replace(/\n/g, ''))
+    <span class = "letter">
+        ${correctLetters.includes(letter) ? letter : ''}
+    </span>
 
-    if(innerWord == selectedWord) {
-        finalMessage.innerText = 'Congratulations! You won!'
+    `).join('')}
+
+    `
+    const innerWord=wordEl.innerText.replace(/\n/g, '')
+
+    if(innerWord == selectedWord){
+        finalMessage.innerText = 'Congratulations You won!'
+        popup.style.display = 'flex'
+        GameEnding = true;
+    }
+}
+
+function updateWrongLettersEl(){
+    wrongLettersEl.innerHTML= `
+    ${wrongLetters.length > 0 ? '<p>wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}<span>`)}
+    `
+
+    figureParts.forEach((part,index) => {
+        const errors =wrongLetters.length
+
+        if(index < errors){
+            part.style.display ='block'
+        }else{
+            part.style.display = 'none'
+        }
+    })
+
+    if(wrongLetters.length == figureParts.length){
+        finalMessage.innerText = 'Unfortunatly you lost :( the word was: ' + selectedWord + '!'
         popup.style.display = 'flex'
     }
 }
 
-//udpate the wrong letters
-function updateWrongLettersEl() {
-    wrongLettersEl.innerHTML() = `
-        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
-        ${wrongLetters.map(letter => `<span>${letter}</span>)}
-        `
-}
 
-//display parts
-figureParts.forEach((part, index) => {
-    const error = wrongLe
-})
-
-//Show Notification
-function showNotification() {
+//show notif
+function showNotification(){
     notification.classList.add('show')
 
-    setTimeout(() => {
+    setTimeout(() =>{
         notification.classList.remove('show')
-
-    })
+    }, 2000)
 }
 
 //keydown letter press
-window.addEventListener('keydown', e => {
+window.addEventListener('keydown', e=>{
+    if (gameEnding) return;
 
-    if(e.keyCode >= 65 && e.keyCode <= 90) {
+    console.log(e.keyCode)
+    if (e.keyCode >= 65 && e.keyCode <=90){
         const letter = e.key
 
         if(selectedWord.includes(letter)){
-            if(!correctLetters.includes(letter)) {
+            if( !correctLetters.includes(letter)){
                 correctLetters.push(letter)
 
+                displayWord()
+            }else{
+                showNotification()
+            }
+        } else{
+            if (!wrongLetters.includes(letter)){
+                wrongLetters.push(letter)
+
                 updateWrongLettersEl()
-            } else {
+            }else{
                 showNotification()
             }
         }
     }
 })
+
+playAgainBtn.addEventListener('click', () => {
+    gameEnding = false;
+    correctLetters.length = 0
+    wrongLetters.length = 0
+    console.log('u clicked')
+    selectedIndex = Math.floor(word.length * Math.random())
+    selectedWord = word[selectedIndex]
+
+    displayWord()
+
+    updateWrongLettersEl()
+
+    popup.style.display = 'none'
+})
+
 displayWord()
