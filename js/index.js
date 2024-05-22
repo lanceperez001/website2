9f0ffc93fe6c2d3ec00dc6062e2bc9017faf469d
Lance Perez <142560197+lanceperez001@users.noreply.github.com>
2024-05-21 19:48:33 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
+nav = document.querySelector('.nav')
 
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if (window.scrollY > 223) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
